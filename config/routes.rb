070ccbe825b7c82397392c319ee2bdbd8cbb1087
magas4891 <magasishe@gmail.com>
2021-01-14 18:32:05 +0200

    Init

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
+  resources :projects
 
   devise_for :users
-  root to: 'home#index'
+  root to: 'projects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
